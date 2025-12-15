7eeaaa1298f34ddc3a4f8ed8a653d0c6d51cab01
xuejun <3145305872@qq.com>
2025-12-15 12:13:38 +0800

    Update MAX_SEQ_COUNT constant to increase the maximum sequence count from 50 to 5000 for improved handling of larger datasets.

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@ import { speciesModelConfigs } from "./config/speciesModels";
 
 export const speciesOptions: string[] = speciesModelConfigs.map((item) => item.name);
 
-export const MAX_SEQ_COUNT = 50;
+export const MAX_SEQ_COUNT = 5000;
 export const MAX_FILE_SIZE = 1 * 1024 * 1024;
 export const MAX_SEQ_LENGTH = 200;
 
